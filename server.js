e542a464bb8a76308fe0bd801b3f96b1690e9cf0
gabypega1997 <gabypega1997@gmail.com>
2022-06-08 19:11:33 +0200

    add postgres folder

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ const image = require('./controllers/image');
 const db = knex({
   // connect to your own database here:
   client: 'pg',
-  connection: process.env.POSTGRES_URI
+  connection: {
+    host : process.env.POSTGRES_HOST,
+    user : process.env.POSTGRES_USER,
+    password : process.env.POSTGRES_PASSWORD,
+    database : process.env.POSTGRES_DB
+  }
 });
 
 const app = express();
